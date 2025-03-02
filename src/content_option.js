059b7719ddb6486ce750102a469a2194de24ee87
Ayush Singh <ayushsinghgaur2100@gmail.com>
2025-03-02 08:30:29 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,125 @@
+const logotext = "PORTFOLIO";
+const meta = {
+    title: "Ayush Singh",
+    description: "Hi I'm Ayush Singh,a passionate web developer and designer from india.",
+};
+
+const introdata = {
+    title: "I’m Ayush Singh",
+    animated: {
+        first: "I Am a Web Developer",
+        second: "Video Editor",
+        third: "and a Web Designer",
+    },
+    description: "I am a web developer and designer from india. I enjoy taking complex problems and turning them into simple and beautiful interface designs. I also love the logic and structure of coding and always strive to write elegant and efficient code whether it be HTML,CSS,Js or REACT.js.",
+    your_img_url: "https://i.postimg.cc/fLVsjHFw/me2.jpg",
+};
+
+const dataabout = {
+    title: "a bit about my self",
+    aboutme: "I am a student in Bachelors Of Computer Application at Kanpur Institute of Technology.I am a Good learner with a positive attitude and a passion for web development and design. I am always eager to learn new technologies and improve my skills. I am a hardworking person who can work in a team as well as individually. I am a quick learner and always ready to take new challenges,I am currently Learning Backend Developement and looking Forward for a Role as a Software Engineer.",
+};
+const worktimeline = [{
+        jobtitle: "Web Developement Intern",
+        where: "Codsoft",
+        date: "2024",
+    }
+];
+
+const skills = [{
+        name: "Python",
+        value: 85,
+    },
+    {
+        name: "React.js",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Java",
+        value: 85,
+    },
+    {
+        name: "DSA with java",
+        value: 83,
+    },
+    {
+        name: "Java Database Connectivity",
+        value: 90,
+    },
+    {
+        name: "C/C++",
+        value: 90,
+    }
+];
+
+const services = [{
+        title: "Web Apps",
+        description: "I can build web applications with the latest technologies and frameworks like React.js, Node.js, and Express.js.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "I can design and develop custom WordPress websites with a unique and modern design.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.pinimg.com/736x/ba/97/83/ba97839b2dbd39bf359c3eb673554d66.jpg",
+        description: "A simple online web music library where you can listen to music online like Spotify.",
+        link: "https://github.com/Ayush-singh141/spotify_clone_online_web_music_library.git",
+    },
+    {
+        img: "https://i.pinimg.com/736x/01/08/18/0108187157bbbfb52a411cbd7143694a.jpg",
+        description: "A Notes app where you can write your notes and save them using PHP.",
+        link: "https://github.com/Ayush-singh141/notes-app-using-PHP-and-SQL.git",
+    },
+    {
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/0f3ed952323519.5608d8fce47b2.png",
+        description: "A helpful assistant like Jarvis using python",
+        link: "https://github.com/Ayush-singh141/Jarvis_Using_Python.git",
+    },
+    {
+        img: "https://i.pinimg.com/474x/15/1e/f8/151ef8698520ec78878cfd54b4211690.jpg",
+        description: "An AI Chat Bot like chat gpt using pure HTML,CSS and JS.",
+        link: "https://github.com/Ayush-singh141/ANUBIS_AI_APP_V1.5.git",
+    },
+    {
+        img: "https://cdn.dribbble.com/userupload/13167768/file/original-08e29755d8f12fdb9ef53d5b88bfeef0.jpg?resize=752x&vertical=center",
+        description: "A simple terminal based chatbot using Java.",
+        link: "https://github.com/Ayush-singh141/Artificial_Intelligence_Chatbot_Using_Java.git",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ayushsinghgaur3145@gmail.com",
+    YOUR_FONE: "(+91) 979-458-0488",
+    description: "You Can Contact me for any query,if you want to hire me for your project or want to work as a team on a project on my team.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_skojfjm",
+    YOUR_TEMPLATE_ID: "template_h8pbum2",
+    YOUR_USER_ID: "85o6X0xlV2LD1lmzn",
+};
+
+const socialprofils = {
+    github: "https://github.com/Ayush-singh141",
+    facebook: "https://www.facebook.com/profile.php?id=61573087858701",
+    linkedin: "https://www.linkedin.com/in/ayush-singh-a619622a9/",
+    twitter: "https://x.com/BATTLEPRIM39865?t=jqB0RcfmvjlCcRCrtPxcPw&s=09",
+    instagram: "https://www.instagram.com/777one4one/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
